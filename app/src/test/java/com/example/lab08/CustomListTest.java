0ca04017cf5e38eb39fadad1997e1c780604327d
jiutianxvanyin <kehan4@ualberta.ca>
2025-10-29 17:36:13 -0600

    count test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -20,5 +20,16 @@ public class CustomListTest {
         list.deleteCity(edmonton);
         assertFalse(list.hasCity(edmonton));
     }
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(0, list.countCities());
+
+        list.addCity(new City("Calgary", "AB"));
+        list.addCity(new City("Edmonton", "AB"));
+
+        assertEquals(2, list.countCities());
+    }
+
 
 }
