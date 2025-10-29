4a2611f2a123e120f29006e500106de8ba843688
jiutianxvanyin <kehan4@ualberta.ca>
2025-10-29 17:12:54 -0600

    a test comment

--- app/src/main/java/com/example/lab08/City.java
+++ app/src/main/java/com/example/lab08/City.java
@@ -11,7 +11,7 @@ public class City {
 
     public String getCityName(){
         return this.city;
-    }
+    } //a test comment
 
     public String getProvinceName(){
         return this.province;
